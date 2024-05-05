0d9af0cb1d6f31482b7d2c7969592bbf7dd5b376
Kenichi Kubota <84975057+Ken1Kubota@users.noreply.github.com>
2024-05-05 13:33:18 +0900

    chore: change to forked oversteer

--- build/oversteer/build_oversteer.sh
+++ build/oversteer/build_oversteer.sh
@@ -3,11 +3,14 @@
 set -ouex pipefail
 
 WORKDIR=/tmp/build/oversteer
+export MESON_INSTALL_PREFIX=/usr
 cd ${WORKDIR}
-git clone https://github.com/berarma/oversteer.git ${WORKDIR}/oversteer
+#git clone https://github.com/berarma/oversteer.git ${WORKDIR}/oversteer
+git clone https://github.com/gotzl/oversteer.git ${WORKDIR}/oversteer
 cd oversteer
 
 patch -u meson_options.txt < ${WORKDIR}/meson_options.txt.patch
+patch -u meson.build < ${WORKDIR}/meson.build.patch
 
 meson  build --prefix /usr
 cd build
